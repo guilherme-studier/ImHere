ee79e6d9889e8ef598bb2500a488c9dd4af7dcb6
Guilherme <guilherme.studier18@gmail.com>
2024-01-22 00:08:03 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { styles } from './styles'
+
+import { Participant } from '../../components/Participant'
+
+export default function Home() {
+  const [participants, setParticipants ] = useState(['João']);
+
+  function handleParticipantAdd() {
+    if (participants.includes('Rodrigo')) return Alert.alert('Participante já existe', 'Já existe um participante com este nome na listagem')
+
+    setParticipants(prevState => [...prevState, 'Ana']);
+  }
+
+  function handleParticipantRemove(name: string) {
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.event}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Terça-feira, 16 de janeiro de 2024.</Text>
+
+      <View style={styles.form}>
+        <TextInput style={styles.input} placeholder='Nome do evento' placeholderTextColor='#6b6b6b' />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.  
+          </Text>
+        )}
+      />
+
+    </View>
+  );
+}
---
+++ teste.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from 'react-native'
+
+export default function App() {
+  return (
+    <View>
+      <Text>React Native</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1310160',
+    padding: 24
+  }
+})
